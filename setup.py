71d44991908ffe32b38beae621b085e342bbb940
Benjamin Kircher <benjamin.kircher@gmail.com>
2017-01-02 22:32:11 +0100

    Add setup.py, missing spec files, Travis CI script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='python-rpm-spec',
+    packages=['rpm'],
+    version='0.1',
+    description='module for parsing RPM spec files.',
+    author='Benjamin Kircher',
+    author_email='benjamin.kircher@gmail.com',
+    url='https://github.com/bkircher/python-rpm-spec',  # use the URL to the GitHub repo
+    download_url='https://github.com/bkircher/python-rpm-spec/archive/v0.1.tar.gz',
+    keywords=['RPM', 'parser', 'spec'],  # arbitrary keywords
+    classifiers=[],
+)
