c715c3343c34911099800cba2ec1e3095ebfe1ab
Benjamin Kircher <benjamin.kircher@gmail.com>
2017-01-22 22:12:38 +0100

    New version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='python-rpm-spec',
     packages=['rpm'],
-    version='0.1',
+    version='0.2',
     description='Module for parsing RPM spec files.',
     author='Benjamin Kircher',
     author_email='benjamin.kircher@gmail.com',
     url='https://github.com/bkircher/python-rpm-spec',  # use the URL to the GitHub repo
-    download_url='https://github.com/bkircher/python-rpm-spec/archive/0.1.tar.gz',
+    download_url='https://github.com/bkircher/python-rpm-spec/archive/0.2.tar.gz',
     keywords=['RPM', 'parser', 'spec'],  # arbitrary keywords
     classifiers=[],
 )
